6375a5052f4bbed62a25bbf84459b808d6bdd9b5
cmdesai <cmdesai@hotmail.com>
2023-04-25 15:11:58 +0530

    Intializing Data with Spring

---
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -0,0 +1,34 @@
+package guru.springframework.sdjpaintro.bootstrap;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+        System.out.println("ID: " + bookDDD.getId());
+        Book savedDDD = bookRepository.save(bookDDD);
+        System.out.println("ID: " + savedDDD.getId());
+
+        Book bookSIA = new Book("Spring In Action", "234234", "O'reilly");
+        Book savedSIA = bookRepository.save(bookSIA);
+
+        bookRepository.findAll().forEach(book ->{
+            System.out.println("Book ID: " + book.getId());
+            System.out.println("Book Title: " + book.getTitle());
+        });
+
+    }
+}
