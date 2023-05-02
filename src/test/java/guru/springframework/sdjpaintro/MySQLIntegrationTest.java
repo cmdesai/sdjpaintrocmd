e2070aa93caa44a0d92b9c60445cce8a26777b22
cmdesai <cmdesai@hotmail.com>
2023-05-02 15:25:36 +0530

    adding natural primary key

--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,8 +1,10 @@
 package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.AuthorUuid;
+import guru.springframework.sdjpaintro.domain.BookNatural;
 import guru.springframework.sdjpaintro.domain.BookUuid;
 import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
+import guru.springframework.sdjpaintro.repositories.BookNaturalRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import guru.springframework.sdjpaintro.repositories.BookUuidRepository;
 import org.junit.jupiter.api.Test;
@@ -28,6 +30,19 @@ public class MySQLIntegrationTest {
 
     @Autowired
     AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookNaturalRepository bookNaturalRepository;
+
+    @Test
+    void bookNaturalTest() {
+        BookNatural bookNatural = new BookNatural();
+        bookNatural.setTitle("My Book");
+        BookNatural saved = bookNaturalRepository.save(bookNatural);
+
+        BookNatural fetched = bookNaturalRepository.getById(saved.getTitle());
+        assertThat(fetched).isNotNull();
+    }
 
     @Test
     void testBookUuid() {
